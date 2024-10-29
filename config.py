0b003e4b60b465f87ff23433658c33b739b29e8d
Itzmemeowtaro <poonamveesee@gmail.com>
2024-10-29 19:56:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6440021089"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "Bdname")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://meow:meow@meow.a6bo1.mongodb.net/?retryWrites=true&w=majority&appName=meow")
 DB_NAME = os.environ.get("DATABASE_NAME", "maowtaro")
 
 #force sub channel id, if you want enable force sub
